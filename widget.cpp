f254d19ab77ed27a2734dccc574b2baea1acdba8
MovsesHovakimyan <87780196+MovsesHovakimyan@users.noreply.github.com>
2024-02-23 17:09:54 +0400

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -4,8 +4,25 @@
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
+    , m_count(0)
 {
+    m_button = new QPushButton("Click Here",this);
+    m_label = new QLabel;
+    m_layout = new QVBoxLayout(this);
+
+    m_label->setNum(m_count);
+
+    m_layout->addWidget(m_button);
+    m_layout->addWidget(m_label);
+
     ui->setupUi(this);
+
+    connect(m_button, SIGNAL (clicked()), this, SLOT (handleButton()));
+}
+
+void Widget::handleButton()
+{
+    m_label->setNum(++m_count);
 }
 
 Widget::~Widget()
