80a9cff4c24547e4213823cbe68da88de8d55e49
MovsesHovakimyan <87780196+MovsesHovakimyan@users.noreply.github.com>
2024-02-23 17:09:23 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,12 @@
 #include "widget.h"
 
 #include <QApplication>
-#include <QVBoxLayout>
-#include <QPushButton>
-#include <QLabel>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
     w.setWindowTitle("Simple Click Counter");
-
-    int count = 0;
-
-    QVBoxLayout *layout = new QVBoxLayout(&w);
-    QPushButton *button = new QPushButton("Click here");
-    QLabel *label = new QLabel;
-
-    label->setNum(count);
-
-    layout->addWidget(button);
-    layout->addWidget(label);
-
-
     w.show();
     return a.exec();
 }
